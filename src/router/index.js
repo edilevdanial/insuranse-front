93003429ab37306d5a5ae3a9d661c2b1de1abe06
danialedilev <danial.200316@gmail.com>
2022-06-04 01:32:55 +0600

    config router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Main from '../views/index'
 
 Vue.use(VueRouter)
 
 
 const routes = [
+    {path:'/',component:Main}
     // { path: '/foo', component: Foo },
     // { path: '/bar', component: Bar }
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
-    routes // short for `routes: routes`
+    routes
 })
 
 export default router
