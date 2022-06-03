3f73189bf42d4a30177f020bae6285433f36bc10
danialedilev <danial.200316@gmail.com>
2022-06-04 00:19:32 +0600

    add quasar

---
+++ src/quasar.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import './styles/quasar.scss'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  }
+ })
